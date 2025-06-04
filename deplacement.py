9f24e5514396713a568bd805178dbc671c8eed31
LDylanGit <Fubuki-kun@outlook.fr>
2025-06-04 16:31:32 +0200

    Add files via upload

--- deplacement.py
+++ deplacement.py
@@ -16,9 +16,25 @@ def right() :
 def left() :
     my_marty.sidestep(side = 'left', steps = 1, step_length = 20, move_time = 1500)
 
+def forward_case() :
+    my_marty.walk(num_steps = 8, start_foot = 'auto', turn = 0, step_length = 20, move_time = 1500)
+
+def backward_case() :
+    my_marty.walk(num_steps = 8, start_foot = 'auto', turn = 0, step_length = -20, move_time = 1500)
+
+def right_case() :
+    my_marty.sidestep(side = 'right', steps = 9, step_length = 20, move_time = 1500)
+
+def left_case() :
+    my_marty.sidestep(side = 'left', steps = 9, step_length = 20, move_time = 1500)
+
 #Permet de tester les déplacements
 #forward()
 #right()
 #left()
 #backward()
 #stand()
+#forward_case()
+#backward_case()
+#right_case()
+#left_case()
--- interface.py
+++ interface.py
@@ -1,8 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 import sys
-from deplacement import forward, backward, right, left, stand
+from deplacement import forward, backward, right, left, stand, forward_case, backward_case, right_case, left_case
 from martypy import Marty
-my_marty = Marty("wifi","192.168.0.107")
+my_marty = Marty("wifi","192.168.0.107")    #Adresse à adapter
 
 class Window(QWidget) :
     def __init__(self) :
@@ -26,11 +26,28 @@ class Window(QWidget) :
         std.setGeometry(100,100,100,100)
         std.clicked.connect(self.appui_std)
 
+        fw1 = QPushButton('Avant case', self)
+        fw1.setGeometry(100,100,100,100)
+        fw1.clicked.connect(self.appui_fw1)
+        bw1 = QPushButton('Arrière case', self)
+        bw1.setGeometry(100,100,100,100)
+        bw1.clicked.connect(self.appui_bw1)
+        r1 = QPushButton('Droite case', self)
+        r1.setGeometry(100,100,100,100)
+        r1.clicked.connect(self.appui_r1)
+        l1 = QPushButton('Gauche case', self)
+        l1.setGeometry(100,100,100,100)
+        l1.clicked.connect(self.appui_l1)
+
         vbox.addWidget(fw)
         vbox.addWidget(bw)
         vbox.addWidget(r)
         vbox.addWidget(l)
         vbox.addWidget(std)
+        vbox.addWidget(fw1)
+        vbox.addWidget(bw1)
+        vbox.addWidget(r1)
+        vbox.addWidget(l1)
 
         self.setLayout(vbox)
         
@@ -49,6 +66,18 @@ class Window(QWidget) :
 
     def appui_std(self) :
         stand()
+
+    def appui_fw1(self) :
+        forward_case()
+    
+    def appui_bw1(self) :
+        backward_case()
+
+    def appui_r1(self) :
+        right_case()
+
+    def appui_l1(self) :
+        left_case()
 
 app = QApplication([])
 window = Window()
