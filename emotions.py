4fe0b9030c605ca92b5a89b46a8cd7ef08bfac5e
TAHA_AIT-EL-MAATI <144101899+SilentTaha@users.noreply.github.com>
2025-06-04 16:44:41 +0200

    Update emotions.py V1.2
    -fix (non-returning to main color): the robot eyes stay on a given color, it don't return to green (ROBOT DEFAULT COLOR ) when the emotion function finished.

--- emotions.py
+++ emotions.py
@@ -4,51 +4,56 @@ from martypy import Marty
 MARTY_IP = "192.168.0.107"  # Update this if the IP changes
 
 # Connect to Marty
-marty = Marty("wifi", MARTY_IP)
+my_marty = Marty("wifi", MARTY_IP)
 
 
 #  Eye expression functions
 
 def eyes_normal():
     
-    marty.eyes("normal")
+    my_marty.eyes("normal")
     print("Normal eyes")
-    marty.disco_color("green")
+    my_marty.disco_color("green")
     time.sleep(2)
+    my_marty.disco_color("green")
 
 def eyes_angry():
-    marty.disco_color("red")
-    marty.eyes("angry")
+    my_marty.disco_color("red")
+    my_marty.eyes("angry")
     print("Angry eyes")
     time.sleep(2)
+    my_marty.disco_color("green")
 
 def eyes_excited():
    
-    marty.disco_color("blue")
-    marty.eyes("excited")
+    my_marty.disco_color("blue")
+    my_marty.eyes("excited")
     print("Excited eyes")
     time.sleep(2)
+    my_marty.disco_color("green")
 
 def eyes_wide():
-    marty.disco_color("green")
-    marty.eyes("wide")
+    my_marty.disco_color("green")
+    my_marty.eyes("wide")
     print("Wide open eyes")
     
+    
     for i in range(4):
-        marty.disco_color("blue")
+        my_marty.disco_color("blue")
         print("set to blue")
         time.sleep(0.4)
-        marty.disco_color("green")
+        my_marty.disco_color("green")
         print("set to green")
         time.sleep(0.2)
         
-    marty.disco_color("green")
+    my_marty.disco_color("green")
     
 def eyes_wiggle():
     
-    marty.eyes("wiggle")
+    my_marty.eyes("wiggle")
     print("Wiggling eyes")
     time.sleep(2)
+    my_marty.disco_color("green")
     
 def applyEmotion(emotionName):
     if emotionName == "normal":
@@ -61,4 +66,3 @@ def applyEmotion(emotionName):
         eyes_wide()
     elif emotionName == "wiggle":
         eyes_wiggle()
-
