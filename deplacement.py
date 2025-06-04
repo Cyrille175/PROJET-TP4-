f5e5e6caf58fee3a651753421948a5f5d3aef46e
LDylanGit <Fubuki-kun@outlook.fr>
2025-06-04 14:41:04 +0200

    Add files via upload

--- deplacement.py
+++ deplacement.py
@@ -1,26 +1,24 @@
 from martypy import Marty
-my_marty = Marty("wifi","192.168.0.103")    #Adresse à adapter
+my_marty = Marty("wifi","192.168.0.107")    #Adresse à adapter
 
 def forward() :
     my_marty.stand_straight(move_time = 2000)
-    my_marty.walk(num_steps = 8, start_foot = 'auto', turn = 0, step_length = 20, move_time = 1500)
+    my_marty.walk(num_steps = 1, start_foot = 'auto', turn = 0, step_length = 20, move_time = 1500)
 
 def backward() :
     my_marty.stand_straight(move_time = 2000)
-    my_marty.walk(num_steps = 8, start_foot = 'auto', turn = 0, step_length = -20, move_time = 1500)
+    my_marty.walk(num_steps = 1, start_foot = 'auto', turn = 0, step_length = -20, move_time = 1500)
 
-def turn_right() :
+def right() :
     my_marty.stand_straight(move_time = 2000)
-    my_marty.walk(num_steps = 4, start_foot = 'auto', turn = -15, step_length = 0, move_time = 1500)
+    my_marty.sidestep(side = right, steps = 1, step_length = 20, move_time = 1500)
 
-def turn_left() :
+def left() :
     my_marty.stand_straight(move_time = 2000)
-    my_marty.walk(num_steps = 4, start_foot = 'auto', turn = 15, step_length = 0, move_time = 1500)
+    my_marty.sidestep(side = left, steps = 1, step_length = 20, move_time = 1500)
 
 #Permet de tester les déplacements
-"""
-forward()
-turn_right()
-turn_left()
-backward()
-"""
+#forward()
+#right()
+#left()
+#backward()
