3701523998557a90108460ea48e5d9cdcbcb51ce
TAHA_AIT-EL-MAATI <144101899+SilentTaha@users.noreply.github.com>
2025-06-04 16:22:26 +0200

    Update emotions.py V1.1
    -add apply function that call other function by their name as argument
    -adding colors effect to eyes when running emotion

--- emotions.py
+++ emotions.py
@@ -4,43 +4,61 @@ from martypy import Marty
 MARTY_IP = "192.168.0.107"  # Update this if the IP changes
 
 # Connect to Marty
-try:
-    marty = Marty("wifi", MARTY_IP)
-except Exception as e:
-    print(f"Connection failed: {e}")
-    exit()
+marty = Marty("wifi", MARTY_IP)
+
 
 #  Eye expression functions
 
 def eyes_normal():
-   
+    
     marty.eyes("normal")
     print("Normal eyes")
+    marty.disco_color("green")
     time.sleep(2)
 
 def eyes_angry():
-   
+    marty.disco_color("red")
     marty.eyes("angry")
     print("Angry eyes")
     time.sleep(2)
 
 def eyes_excited():
    
+    marty.disco_color("blue")
     marty.eyes("excited")
     print("Excited eyes")
     time.sleep(2)
 
 def eyes_wide():
-    
+    marty.disco_color("green")
     marty.eyes("wide")
     print("Wide open eyes")
-    time.sleep(2)
-
+    
+    for i in range(4):
+        marty.disco_color("blue")
+        print("set to blue")
+        time.sleep(0.4)
+        marty.disco_color("green")
+        print("set to green")
+        time.sleep(0.2)
+        
+    marty.disco_color("green")
+    
 def eyes_wiggle():
     
     marty.eyes("wiggle")
     print("Wiggling eyes")
     time.sleep(2)
     
-    
-eyes_wiggle()
+def applyEmotion(emotionName):
+    if emotionName == "normal":
+        eyes_normal()
+    elif emotionName == "angry":
+        eyes_angry()
+    elif emotionName == "excited":
+        eyes_excited()
+    elif emotionName == "wide":
+        eyes_wide()
+    elif emotionName == "wiggle":
+        eyes_wiggle()
+
