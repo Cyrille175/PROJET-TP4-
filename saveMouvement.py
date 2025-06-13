78b21a03ec4555886217ba82c391a7848d171fb8
TAHA_AIT-EL-MAATI <144101899+SilentTaha@users.noreply.github.com>
2025-06-13 10:29:18 +0200

    for test
    -has a problem always save last move ( a step behind )

--- saveMouvement.py
+++ saveMouvement.py
@@ -9,30 +9,30 @@ direction_codes = {
 
 # Variables globales pour memoriser le dernier mouvement
 last_move = None
-move_count = 0
+move_count = 1
 
 def save_movement(direction):
     """
-    Enregistre les mouvements successifs compresses, ex : 2R, 3F...
+    Enregistre les mouvements successifs, ex : 2R, 3F...
     """
     global last_move, move_count
 
     if direction not in direction_codes:
-        return
+        return #rien
 
     if direction == last_move:
         move_count += 1
     else:
         if last_move is not None:
             # Sauvegarder le mouvement precedent compresse
-            code = direction_codes[last_move]
+            code = direction_codes[direction]
             with open("mouvement_path.txt", "a", encoding="utf-8") as f:
                 f.write(f"{move_count}{code}\n")
         # Nouveau mouvement
         last_move = direction
         move_count = 1
 
-def flush_movement():
+def flush_movement(): # quant? optionel (on flush quand on veux(on appel a l'interieru de n'import quel fonction))
     """
     a appeler a  la fin (fermeture, bouton stop...) pour enregistrer le dernier mouvement
     """
